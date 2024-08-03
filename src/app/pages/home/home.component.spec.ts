0ba72b45790cbecafd576751487d7a4bc14b0921
Gustavo <guzanony1234@gmail.com>
2024-08-03 01:31:16 -0300

    testes unitários

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,14 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { HttpClientModule } from '@angular/common/http';
 
-describe('HomeComponent', () => {
+fdescribe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HomeComponent]
+      declarations: [HomeComponent],
+      imports: [HttpClientModule]
     });
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
@@ -18,4 +20,12 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should test getPromotions', () => {
+    component['_promotionService'].getPromotions = jasmine.createSpy();
+    component['getPromotions']();
+    component['_promotionService'].getPromotions().subscribe((resp) => {
+      expect(component.promotions).toEqual(resp)
+    })
+  })
 });
